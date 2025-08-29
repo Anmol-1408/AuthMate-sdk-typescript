1f20a53f7575588c82b7665ea9f7eb9f9a2320ac
“Anmol” <pal592anmol@gmail.com>
2025-08-29 20:07:31 +0530

    Bug : fixed export

--- src/authmate.ts
+++ src/authmate.ts
@@ -1,5 +1,6 @@
 export * from './api/authmateClient';
 export * from './components/ProtectedRoute';
+export * from './components/LoggedIn';
 export * from './hooks/useAuth';
 export * from './storage/tokenStorage';
 export * from './types';
